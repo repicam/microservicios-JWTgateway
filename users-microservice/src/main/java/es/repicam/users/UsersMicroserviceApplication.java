6a1be5ac0c26c7768016089e4fe3b80fcd40cb1f
Ivan Repiso Camara <irepisocamara@gmail.com>
2024-02-22 09:44:56 +0100

    Eureka server configuration

--- users-microservice/src/main/java/es/repicam/users/UsersMicroserviceApplication.java
+++ users-microservice/src/main/java/es/repicam/users/UsersMicroserviceApplication.java
@@ -2,9 +2,11 @@ package es.repicam.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UsersMicroserviceApplication {
 
