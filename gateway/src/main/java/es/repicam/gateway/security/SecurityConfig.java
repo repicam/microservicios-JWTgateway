bf61ff40be6abaaa152e0a98a909a5e3f3956260
Ivan Repiso Camara <irepisocamara@gmail.com>
2024-02-25 23:17:33 +0100

    Keycloak configuration and security filters

---
+++ gateway/src/main/java/es/repicam/gateway/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package es.repicam.gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
