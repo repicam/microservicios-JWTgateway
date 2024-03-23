5a0f1d33af939b9493d19483959bdb41dd4e012e
Ivan Repiso Camara <irepisocamara@gmail.com>
2024-03-23 12:42:08 +0100

    Fallback response

---
+++ users-microservice/src/main/java/es/repicam/users/dto/UserResponseFallback.java
@@ -0,0 +1,24 @@
+package es.repicam.users.dto;
+
+import es.repicam.users.entity.User;
+import es.repicam.users.model.Book;
+import es.repicam.users.model.Film;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseFallback {
+
+    private String username;
+    private String id;
+
+    private String error;
+}
